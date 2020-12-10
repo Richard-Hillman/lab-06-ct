ee793e63858d4820fc5c7b20a2537f2e5befb846
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-12-10 15:46:43 -0800

    added cybertruckjs added crud routes added models

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,24 +3,39 @@ const app = require('../lib/app');
 const pool = require('../lib/utils/pool');
 const fs = require('fs');
 
+// ----------------------------------------
+
 describe('app test', () => {
+
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
+
   afterAll(() => {
     return pool.end();
   });
+
+  // ---------------------------------------
+
   it('new cybertruck', async() => {
+
     const response = await request(app)
       .post('/api/v1/tesla/cybertruck')
       .send({
         color: 'red',
         type: 'truck'
       });
+
+    // --------------
+
     expect(response.body).toEqual({
       id: '1',
       color: 'red',
       type: 'truck'
     });
+
   });
+
+  // --------------------------------------
+
 });
